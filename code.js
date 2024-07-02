dfa35bb6b4f803d804cf1fcce6f0039878a92454
Gustavo Mello <gustavomello9178@gmail.com>
2024-07-02 16:08:03 -0300

    feat(code-page): Conversão das variaveis de str para int

--- code.js
+++ code.js
@@ -1,3 +1,9 @@
 aluno1 = prompt("Digite a nota do primeiro aluno!")
 aluno2 = prompt("Digite a nota do segundo aluno!")
 aluno3 = prompt("Digite a nota do terceiro aluno!")
+
+aluno1 = parseInt(aluno1)
+aluno2 = parseInt(aluno2)
+aluno3 = parseInt(aluno3)
+
+
